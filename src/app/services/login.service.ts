b432b6b7fdaac1358e2cf169e349371a4df512e4
Jonas D <77809544+Otodekotto@users.noreply.github.com>
2023-01-30 18:39:10 +0100

    bug fix
    Fix: omport in login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of, switchMap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
 
-const { apiUsers , apiKey} = environment;
+const { apiURL , apiKey} = environment;
 
 @Injectable({
   providedIn: 'root'
