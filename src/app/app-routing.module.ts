96b56bc554607b4d0a228c0184ea515d54112480
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-01-31 15:07:53 +0100

    feat: pokemin interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,19 @@
-import { LoginPage } from "./pages/login/login.page";
-import {NgModule} from "@angular/core"
-import { RouterModule , Routes } from "@angular/router";
+import { LoginPage } from './pages/login/login.page';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 const routes: Routes = [
-    
-    {
+  {
     path: '',
-    component: HeaderComponent,
-    },
-    {
-        path:"",
-        pathMatch: "full",
-        redirectTo: "/login"
-    },
-    {
-        path:"login",
-        component: LoginPage
-    },
+    pathMatch: 'full',
+    redirectTo: '/login',
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
   {
     path: 'pokemon-catalogue',
     component: PokemonCataloguePage,
@@ -27,17 +22,10 @@ const routes: Routes = [
     path: 'profile',
     component: PokemonCataloguePage,
   },
-]
+];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ], //import a module
-    exports: [
-        RouterModule
-    ] // exposde module and it's features
+  imports: [RouterModule.forRoot(routes)], //import a module
+  exports: [RouterModule], // exposde module and it's features
 })
-
-export class AppRoutingModule{
-
-}
+export class AppRoutingModule {}
