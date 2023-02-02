3b01f122986cff67557c423a5ade70ec2183d79c
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-02-02 16:26:11 +0100

    feat: catch button function saves user pokemon

--- src/app/components/info-card-button/info-card-button.component.ts
+++ src/app/components/info-card-button/info-card-button.component.ts
@@ -1,26 +1,37 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { TrainerService } from 'src/app/services/trainer.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-info-card-button',
   templateUrl: './info-card-button.component.html',
   styleUrls: ['./info-card-button.component.css'],
 })
-export class InfoCardButtonComponent {
+export class InfoCardButtonComponent implements OnInit {
+  public isCaught: boolean = false;
+
   @Input() pokemonName: string = '';
 
   get loading(): boolean {
     return this.trainerService.loading;
   }
 
-  constructor(private readonly trainerService: TrainerService) {}
+  constructor(
+    private userService: UserService,
+    private readonly trainerService: TrainerService
+  ) {}
+
+  ngOnInit(): void {
+    this.isCaught = this.userService.inPokemons(this.pokemonName);
+  }
 
   onButtonClick(): void {
     //Add the pokemon to trainer
     this.trainerService.addToTrainer(this.pokemonName).subscribe({
-      next: (response: any) => {
-        console.log('Next', response);
+      next: (user: User) => {
+        this.isCaught = this.userService.inPokemons(this.pokemonName);
       },
       error: (error: HttpErrorResponse) => {
         console.log('Error', error);
